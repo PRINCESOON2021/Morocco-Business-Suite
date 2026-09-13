63bc3d7c37234dea8da47f63564adebd16cfff27
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 12:51:45 +0100

    feat: add MBS core manifest

---
+++ addons/mbs_core/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "MBS Core Maroc",
+    "version": "18.0.1.0.0",
+    "summary": "Socle metier Maroc pour Morocco Business Suite",
+    "category": "Localization",
+    "license": "LGPL-3",
+    "depends": ["base", "contacts", "l10n_ma"],
+    "data": [
+        "views/res_company_views.xml",
+        "views/res_partner_views.xml"
+    ],
+    "installable": True,
+    "application": False
+}
