84846b402852e36cfd50a22fcc219b57499cc641
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 12:56:38 +0100

    refactor(core): load global MBS menu from core

--- addons/mbs_core/__manifest__.py
+++ addons/mbs_core/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "MBS Core Maroc",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "summary": "Socle metier Maroc pour Morocco Business Suite",
     "category": "Localization",
     "license": "LGPL-3",
     "depends": ["base", "contacts", "l10n_ma"],
     "data": [
+        "views/menus.xml",
         "views/res_company_views.xml",
         "views/res_partner_views.xml"
     ],
