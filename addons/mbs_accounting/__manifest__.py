4e2fd14c8d45ab158c6e89d4cdbf54f5b396476f
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 12:52:33 +0100

    feat: add accounting module manifest

---
+++ addons/mbs_accounting/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "MBS Comptabilite Maroc",
+    "version": "18.0.1.0.0",
+    "summary": "Comptabilite marocaine, controles et reporting",
+    "category": "Accounting/Accounting",
+    "license": "LGPL-3",
+    "depends": ["mbs_core", "mbs_commercial", "account"],
+    "data": ["views/account_move_views.xml"],
+    "installable": True,
+    "application": True
+}
