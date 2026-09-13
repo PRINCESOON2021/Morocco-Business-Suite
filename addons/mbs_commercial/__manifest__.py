eda9eedabad2ae55cf5c81135cdd1c71555fe59f
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 12:52:23 +0100

    feat: wire commercial flows on Odoo Community

---
+++ addons/mbs_commercial/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "MBS Gestion Commerciale Maroc",
+    "version": "18.0.1.0.0",
+    "summary": "Ventes, achats, stock et facturation pour Morocco Business Suite",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "depends": [
+        "mbs_core",
+        "sale_management",
+        "purchase_stock",
+        "stock_account",
+        "account"
+    ],
+    "data": [],
+    "installable": True,
+    "application": True
+}
