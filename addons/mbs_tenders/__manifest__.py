9b39102c76cf9ee34d41ad05ee39c06a15f0826f
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 12:53:16 +0100

    feat: add tenders module manifest

---
+++ addons/mbs_tenders/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "MBS Marches et Appels d'Offres Maroc",
+    "version": "18.0.0.1.0",
+    "summary": "Gestion des marches, lots, attachements, situations et decompte",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "depends": ["mbs_commercial", "mbs_accounting"],
+    "data": [],
+    "installable": True,
+    "application": True
+}
