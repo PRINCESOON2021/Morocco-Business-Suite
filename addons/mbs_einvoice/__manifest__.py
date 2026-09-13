f65a4f9f7cb93cf05f067b1a5fee041d4944a1c4
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 12:53:07 +0100

    feat: add e-invoice module manifest

---
+++ addons/mbs_einvoice/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "MBS Facturation Electronique Maroc",
+    "version": "18.0.0.1.0",
+    "summary": "Architecture de connecteur e-facture Maroc versionnee",
+    "category": "Accounting/Accounting",
+    "license": "LGPL-3",
+    "depends": ["mbs_accounting"],
+    "data": [],
+    "installable": True,
+    "application": False
+}
