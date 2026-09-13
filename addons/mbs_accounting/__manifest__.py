f8bac7975d1d08b9fcae0ac55c04cfec61c73723
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 13:00:59 +0100

    feat(accounting): enable company situation dashboard

--- addons/mbs_accounting/__manifest__.py
+++ addons/mbs_accounting/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "MBS Comptabilite Maroc",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.2.0",
     "summary": "Comptabilite marocaine, controles et reporting",
     "category": "Accounting/Accounting",
     "license": "LGPL-3",
@@ -8,7 +8,8 @@
     "data": [
         "security/ir.model.access.csv",
         "views/account_move_views.xml",
-        "views/report_wizard_views.xml"
+        "views/report_wizard_views.xml",
+        "views/business_dashboard_views.xml"
     ],
     "installable": True,
     "application": True
