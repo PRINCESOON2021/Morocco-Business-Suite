11c4c96c480249a5737a73cab1f4928baee68c0f
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 12:55:20 +0100

    feat(accounting): enable reporting views and access

--- addons/mbs_accounting/__manifest__.py
+++ addons/mbs_accounting/__manifest__.py
@@ -1,11 +1,15 @@
 {
     "name": "MBS Comptabilite Maroc",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "summary": "Comptabilite marocaine, controles et reporting",
     "category": "Accounting/Accounting",
     "license": "LGPL-3",
     "depends": ["mbs_core", "mbs_commercial", "account"],
-    "data": ["views/account_move_views.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_move_views.xml",
+        "views/report_wizard_views.xml"
+    ],
     "installable": True,
     "application": True
 }
