ba17e6b7dcd8f4e7630472cd94a3deb8e11df336
PRINCESOON2021 <86659002+PRINCESOON2021@users.noreply.github.com>
2026-09-13 12:59:38 +0100

    feat(einvoice): enable electronic invoicing workflow UI

--- addons/mbs_einvoice/__manifest__.py
+++ addons/mbs_einvoice/__manifest__.py
@@ -1,11 +1,13 @@
 {
     "name": "MBS Facturation Electronique Maroc",
-    "version": "18.0.0.1.0",
+    "version": "18.0.1.0.0",
     "summary": "Architecture de connecteur e-facture Maroc versionnee",
     "category": "Accounting/Accounting",
     "license": "LGPL-3",
     "depends": ["mbs_accounting"],
-    "data": [],
+    "data": [
+        "views/account_move_views.xml"
+    ],
     "installable": True,
     "application": False
 }
